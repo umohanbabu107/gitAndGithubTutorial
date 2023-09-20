1940bf29d5deda9abf6f1fa10f0fdfd393641088
Mohan <umohanbabu107@gmail.com>
2023-09-20 10:22:19 +0530

    New files added and existed are modified

--- src/test/java/org/example/MyTest4_Remote.java
+++ src/test/java/org/example/MyTest4_Remote.java
@@ -1,1 +1,5 @@
-// This file is adde from the github remote repository
+package org.example;
+
+public class MyTest4_Remote{
+// This file is added from the github remote repository
+        }
---
+++ src/test/java/org/example/MyTest5_Local.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class MyTest5_Local {
+    //This test is added from the local
+}
